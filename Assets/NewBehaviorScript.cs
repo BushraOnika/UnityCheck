d4e90897c67816c8cc17458f87980f1f4ed0ae84
Bushra Hossain <bushrahossainonika@gmail.com>
2024-11-10 01:37:15 +0600

    KeyBoard Movement

---
+++ Assets/NewBehaviorScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class NewBehaviorScript : MonoBehaviour
+{
+    public float keyinput;
+   
+    public float verticalInput;
+   // public float speed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Left Right movement with keyboard
+        keyinput = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody>().linearVelocity = new Vector3(keyinput,GetComponent<Rigidbody>().linearVelocity.y, 0);
+
+
+       
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Move in both directions Up Down Right Left With keyboard
+        GetComponent<Rigidbody>().linearVelocity = new Vector3(keyinput, verticalInput, 0);
+    }
+}
